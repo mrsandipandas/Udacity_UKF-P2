7c049351bd1135408b47de575e644ad3489b3ae1
s-a-n-d-y <mr.sandipan.das@gmail.com>
2018-12-07 01:02:26 +0100

    Working version

--- src/tools.cpp
+++ src/tools.cpp
@@ -41,26 +41,3 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     //return the result
     return rmse;
 }
-
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-    MatrixXd Hj(3,4);
-    
-    //recover state parameters
-    double px = x_state(0);
-    double py = x_state(1);
-    double vx = x_state(2);
-    double vy = x_state(3);
-
-    //pre-compute a set of terms to avoid repeated calculation
-    const double eps = 0.00001;
-    const double c1 = std::max(eps, px*px + py*py);
-    const double c2 = sqrt(c1);
-    const double c3 = (c1*c2);
-    
-    //compute the Jacobian matrix
-    Hj << (px/c2), (py/c2), 0, 0,
-        -(py/c1), (px/c1), 0, 0,
-        py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
-
-    return Hj;
-}
